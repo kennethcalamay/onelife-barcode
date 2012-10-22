6e1022358127445baf27f4c3b19cf858b51ff134
Kenneth Calamay <github@kennethcalamay.com>
2012-10-22 11:25:55 +0800

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnelifeBarcode::Application.config.secret_token = '4fc76289fa550444f5cae8e0136263ee3b9132510d62d420c285bad365481e31bb85fde56008ea22063bb893e944d1a5cc18fbe1501fcc0e7b62367a4642b89d'
